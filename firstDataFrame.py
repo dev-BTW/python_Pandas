c5a269c29a646cc3c8c7b2072a64c89a19716f8f
dev-BTW <parth.more.2304@gmail.com>
2025-09-07 22:43:01 +0530

    DataFrame from dictionary

---
+++ firstDataFrame.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "name":["Luffy","Zoro","Sanji","Franky","Brook","Chopper","Nami","Robin"],
+    "Gender":["Male","Male","Male","Male","Male","Male","Female","Female"],
+    "Speciality":["Punch","Sword","Kick","Punch","Sword","Meds","Navigate",["Knowledge","Punch"]]
+    }
+
+
+df = pd.DataFrame(data)
+print(df)
